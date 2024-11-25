5745f068fe68dfbdee01419d64d335816e0874f2
devmax214 <toxakasyan7@gmail.com>
2024-11-25 11:46:35 +0200

    Add @module-federation

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,10 +1,27 @@
+import { federation } from "@module-federation/vite";
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
 import { vitePlugin as remix } from "@remix-run/dev";
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix(), 
+    federation({
+      name: "host",
+      remotes: {
+        remote: {
+          type: "module",
+          name: "remote",
+          entry: "http://localhost:3002/remoteEntry.js",
+          entryGlobalName: "remote",
+          shareScope: "default",
+        },
+      },
+      exposes: {},
+      filename: "remoteEntry.js",
+    }),
+    tsconfigPaths()
+  ],
   server: {
     port: 3001,
   },
--- remote/vite.config.ts
+++ remote/vite.config.ts
@@ -1,10 +1,21 @@
+import { federation } from "@module-federation/vite";
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
 import { vitePlugin as remix } from "@remix-run/dev";
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix(), 
+    federation({
+      filename: "remoteEntry.js",
+      name: "remote",
+      exposes: {
+        "./remote-widget1": "./app/components/Widget1.tsx",
+      },
+      remotes: {},
+    }),
+    tsconfigPaths()
+  ],
   server: {
     port: 3002,
   },
